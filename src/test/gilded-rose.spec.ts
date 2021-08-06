25cdab246454b73208cdf2cee44ff006aa230563
nicolasbelfis <nicolas.belfis@codurance.com>
2021-08-06 11:16:42 +0200

    refactor 1

--- src/test/gilded-rose.spec.ts
+++ src/test/gilded-rose.spec.ts
@@ -3,11 +3,11 @@ import {items, printResult} from "./golden-master-text-to-string";
 
 describe('Gilded Rose', function () {
 
-    it('should foo', function() {
-        const gildedRose: GildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).toEqual('fixme');
-    });
+    // it('should foo', function() {
+    //     const gildedRose: GildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
+    //     const items = gildedRose.updateQuality();
+    //     expect(items[0].name).toEqual('fixme');
+    // });
 
     it('compare old and new', () => {
 
